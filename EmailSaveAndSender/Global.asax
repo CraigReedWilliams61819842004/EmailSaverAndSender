3ed6a9ed598b31795410c9f2ddb1711edfe16317
ReedW <ReedW@DESKTOP-9VNGLS7>
2016-11-22 12:41:19 -0600

    Add project files.

---
+++ EmailSaveAndSender/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmailSaveAndSender.MvcApplication" Language="C#" %>
---
+++ EmailSaveAndSender/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailSaveAndSender.Startup))]
+namespace EmailSaveAndSender
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
